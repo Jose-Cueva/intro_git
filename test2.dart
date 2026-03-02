7cb15a1675eccb61240d6ceb124d45c378e366c9
JoseCueva <0801200823258@iteesa.edu.hn>
2026-03-02 12:05:30 -0600

    agregamos nueva logica

--- test2.dart
+++ test2.dart
@@ -1,3 +1,10 @@
 void saludar(){
-  print("Hola 12BTP");
+  print("Hola 12BTP, ya estamos trabajando con github");
+bool poneratencion=false;
+
+  if(poneratencion){
+    print("No se pierden detalles");
+  }else{
+    print("Se pierden detalles");
+  }
 }
